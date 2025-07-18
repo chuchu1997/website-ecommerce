61166494ba9cd48a86f8cacfb91ff3407e5d31cd
cuong <cuong@foxit.co.jp>
2025-07-18 16:19:55 +0700

    handle cart

--- web/src/app/(main)/san-pham/[slug]/components/productClientPC.tsx
+++ web/src/app/(main)/san-pham/[slug]/components/productClientPC.tsx
@@ -96,11 +96,7 @@ export const ProductClientPC = ({ product }: propsProductClientPC) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const userID = cookies.userInfo?.id ?? 999;
-    if (!userID) {
-      toast.error("Vui lòng đăng nhập để thêm sản phẩm vào giỏ hàng");
-      return;
-    }
+    const userID = (await cookies.userInfo?.id) ?? 0;
 
     try {
       const res = await UserCartAPI.getAllCartItemsOfUser(userID);
