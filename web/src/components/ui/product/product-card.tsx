135a73bcba208e4304eda7fac52964db13fec100
cuong <cuong@foxit.co.jp>
2025-07-23 11:46:19 +0700

    fix merge conflic

--- web/src/components/ui/product/product-card.tsx
+++ web/src/components/ui/product/product-card.tsx
@@ -105,81 +105,10 @@ export const ProductCard = ({
   const handleAddToCart = async (e: React.MouseEvent, isCheckout: boolean) => {
     e.preventDefault();
     e.stopPropagation();
-<<<<<<< HEAD
-
-    let userID = cookies.userInfo?.id ?? 0;
-
-    try {
-      const res = await UserCartAPI.getAllCartItemsOfUser(userID);
-
-      if (userID === 0) {
-        setCookie(
-          "userInfo",
-          { id: res.data.cart.userId },
-          {
-            path: "/",
-            maxAge: 60 * 60 * 24 * 365 * 5, // 5 năm
-            sameSite: "lax",
-          }
-        );
-      }
-
-      userID = res.data.cart.userId;
-      // res.data.userID
-
-      const currentItems = Array.isArray(res.data?.cart?.items)
-        ? res.data.cart.items
-        : [];
-
-      const existingIndex = currentItems.findIndex(
-        (item: any) => item.product.id === product.id
-      );
-
-      let updatedItems: CartItemSSR[] = [];
-
-      if (existingIndex !== -1) {
-        updatedItems = currentItems.map((item: any, index: number) =>
-          index === existingIndex
-            ? {
-                ...item,
-                quantity: item.quantity + 1,
-                isSelect: true,
-              }
-            : {
-                ...item,
-                isSelect: false,
-              }
-        );
-      } else {
-        updatedItems = [
-          ...currentItems.map((item: any) => ({
-            ...item,
-            isSelect: false,
-          })),
-          {
-            isSelect: true,
-            product,
-            quantity: 1,
-          },
-        ];
-      }
-
-      await UserCartAPI.updateCartItems(userID, res.data.cart.id, updatedItems);
-
-      setCartQuantity(updatedItems.length);
-      toast.success("Đã thêm sản phẩm vào giỏ hàng");
-      if (isCheckout) {
-        router.push("/checkout");
-      }
-    } catch (error) {
-      toast.error("Có lỗi xảy ra khi thêm sản phẩm vào giỏ hàng");
-    }
-=======
     await addToCart({
       product,
       isCheckout,
     });
->>>>>>> master
   };
   useEffect(() => {
     setIsMounted(true);
