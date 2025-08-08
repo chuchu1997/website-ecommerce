b1206811b46dd509626194062235d92ac40eca74
cuong <cuong@foxit.co.jp>
2025-08-08 15:41:31 +0700

    add

--- api/prisma/migrations/20250808084028_init/migration.sql
+++ api/prisma/migrations/20250808084028_init/migration.sql
@@ -430,11 +430,30 @@ CREATE TABLE `Banner` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Image` (
+CREATE TABLE `ImageMedia` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `url` VARCHAR(191) NOT NULL,
-    `productId` INTEGER NOT NULL,
+    `alt` VARCHAR(191) NULL,
+    `type` ENUM('PRODUCT', 'CATEGORY', 'BANNER', 'BRAND', 'NEWS', 'SERVICE', 'PROJECT', 'NONE') NULL,
+    `size` INTEGER NULL,
+    `width` INTEGER NULL,
+    `height` INTEGER NULL,
+    `productId` INTEGER NULL,
+    `bannerId` INTEGER NULL,
+    `categoryId` INTEGER NULL,
+    `brandId` INTEGER NULL,
+    `newsId` INTEGER NULL,
+    `serviceId` INTEGER NULL,
+    `projectId` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `Image_productId_idx`(`productId`),
+    UNIQUE INDEX `ImageMedia_productId_key`(`productId`),
+    UNIQUE INDEX `ImageMedia_bannerId_key`(`bannerId`),
+    UNIQUE INDEX `ImageMedia_categoryId_key`(`categoryId`),
+    UNIQUE INDEX `ImageMedia_brandId_key`(`brandId`),
+    UNIQUE INDEX `ImageMedia_newsId_key`(`newsId`),
+    UNIQUE INDEX `ImageMedia_serviceId_key`(`serviceId`),
+    UNIQUE INDEX `ImageMedia_projectId_key`(`projectId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
